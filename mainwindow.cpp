9373fdce5e2860680a02541f547987a3e1a146a8
IsmailKonderev6030 <73698568+IsmailKonderev6030@users.noreply.github.com>
2022-03-29 22:42:36 +0300

    HW_1.3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QtMath>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->radioButton_2->setChecked(true);
 }
 
 MainWindow::~MainWindow()
@@ -18,20 +16,17 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    QString resText = "";
-
-    double b = ui->lineEdit->text().toDouble();
-    double c = ui->lineEdit_2->text().toDouble();
-    double y = ui->lineEdit_3->text().toDouble();
-
-    if(ui->radioButton->isChecked())
-        y = y*M_PI/180;
-
-    double res = sqrt(b*b+c*c-2*b*c*cos(y));
-
-    resText.setNum(res);
-
-    ui->lineEdit_4->setText(resText);
-
+    ui->textEdit_2->setText(ui->textEdit_2->toPlainText()+ui->textEdit->toPlainText());
 }
 
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->textEdit->setText(ui->textEdit_2->toPlainText());
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    ui->textEdit->setHtml("<fontcolor='red'>Hello</font>");
+}
